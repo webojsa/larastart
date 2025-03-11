d0fa5d3e1f48401881c47b666e31957424b1529c
Nebojsa <nebojsailic.info@gmail.com>
2025-03-11 14:06:06 +0100

    factories-seeders+

---
+++ database/factories/ProductTagFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductTag>
+ */
+class ProductTagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_id' => Product::factory(),
+            'tag_id' => Tag::factory()
+        ];
+    }
+}
