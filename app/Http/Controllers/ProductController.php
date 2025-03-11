ebaf646c9c345591a416f1de31cb1ca46fa35e18
Nebojsa <nebojsailic.info@gmail.com>
2025-03-11 18:10:44 +0100

    controllers-services

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    protected $productService;
+
+    public function __construct(ProductService $productService){
+        $this->productService = $productService;
+    }
+}
